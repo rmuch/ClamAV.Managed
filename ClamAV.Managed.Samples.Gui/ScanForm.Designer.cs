93a3a797cf12af54dde882a293a06b8e93e872c0
Rupert Muchembled <rupert@rmuch.net>
2013-07-01 15:17:33 +0100

    Import GUI sample sources.

---
+++ ClamAV.Managed.Samples.Gui/ScanForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ClamAV.Managed.Samples.Gui
+{
+    partial class ScanForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scanPathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scanProgressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileOrDirectoryContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.directoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.fileOrDirectoryContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // scanPathTextBox
+            // 
+            this.scanPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scanPathTextBox.Location = new System.Drawing.Point(12, 27);
+            this.scanPathTextBox.Name = "scanPathTextBox";
+            this.scanPathTextBox.Size = new System.Drawing.Size(200, 23);
+            this.scanPathTextBox.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseButton.Location = new System.Drawing.Point(216, 27);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Select";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // scanButton
+            // 
+            this.scanButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.scanButton.Location = new System.Drawing.Point(297, 27);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(75, 23);
+            this.scanButton.TabIndex = 2;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logTextBox.Location = new System.Drawing.Point(12, 141);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(360, 308);
+            this.logTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select a file to scan.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Scan Report";
+            // 
+            // scanProgressBar
+            // 
+            this.scanProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scanProgressBar.Location = new System.Drawing.Point(12, 83);
+            this.scanProgressBar.Name = "scanProgressBar";
+            this.scanProgressBar.Size = new System.Drawing.Size(360, 23);
+            this.scanProgressBar.TabIndex = 6;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 65);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(77, 15);
+            this.statusLabel.TabIndex = 7;
+            this.statusLabel.Text = "Status: Ready";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Title = "Select file or directory to scan...";
+            // 
+            // fileOrDirectoryContextMenuStrip
+            // 
+            this.fileOrDirectoryContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.directoryToolStripMenuItem});
+            this.fileOrDirectoryContextMenuStrip.Name = "fileOrDirectoryContextMenuStrip";
+            this.fileOrDirectoryContextMenuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.fileOrDirectoryContextMenuStrip.Size = new System.Drawing.Size(132, 48);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.fileToolStripMenuItem.Text = "&File...";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // directoryToolStripMenuItem
+            // 
+            this.directoryToolStripMenuItem.Name = "directoryToolStripMenuItem";
+            this.directoryToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.directoryToolStripMenuItem.Text = "&Directory...";
+            this.directoryToolStripMenuItem.Click += new System.EventHandler(this.directoryToolStripMenuItem_Click);
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.Description = "Select a directory to scan...";
+            this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // ScanForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.scanProgressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.scanPathTextBox);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "ScanForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Virus Scanner";
+            this.fileOrDirectoryContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox scanPathTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar scanProgressBar;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ContextMenuStrip fileOrDirectoryContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem directoryToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
---
+++ ClamAV.Managed.Samples.Gui/ScanForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClamAV.Managed.Samples.Gui
+{
+    public partial class ScanForm : Form
+    {
+        private ClamAV _clamAV;
+
+        public ScanForm()
+        {
+            // Windows Forms setup.
+            InitializeComponent();
+
+            this.Icon = SystemIcons.Shield;
+
+            // Instantiate ClamAV engine.
+            _clamAV = new ClamAV();
+
+            logTextBox.AppendText("ClamAV Version: " + _clamAV.Version +"\r\n");
+
+            logTextBox.AppendText("Loading signatures from default location...\r\n");
+
+            // Disable user input while loading.
+            scanPathTextBox.Enabled = false;
+            browseButton.Enabled = false;
+            scanButton.Enabled = false;
+
+            // Load signatures on a new thread.
+            Thread setupThread = new Thread(() =>
+                {
+                    try
+                    {
+                        // Load database from the default location.
+                        _clamAV.LoadDatabase(@"E:\Projects\ClamAV.Managed_Working\ClamAV.Managed.Demo\Database\");
+
+                        // Signal database load is complete.
+                        this.Invoke(new Action(() =>
+                        {
+                            logTextBox.AppendText("Signatures loaded.\r\n");
+
+                            scanPathTextBox.Enabled = true;
+                            browseButton.Enabled = true;
+                            scanButton.Enabled = true;
+                        }));
+                    }
+                    catch (Exception ex)
+                    {
+                        // Report exception to user.
+                        this.Invoke(new Action(() =>
+                        {
+                            logTextBox.AppendText("Failed to load signatures:\r\n");
+                            logTextBox.AppendText(ex.ToString());
+                        }));
+                    }
+                });
+
+            // Begin work.
+            setupThread.Start();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            // Show context menu strip to select between a file and directory to scan.
+            // fileOrDirectoryContextMenuStrip.Show(browseButton.PointToScreen(new Point(1, browseButton.Height)));
+
+            fileToolStripMenuItem_Click(sender, e);
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Browse for a file to scan.
+
+            var dialogResult = openFileDialog.ShowDialog();
+
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                scanPathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void directoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Browse for a folder to scan.
+
+            var dialogResult = folderBrowserDialog.ShowDialog();
+
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                scanPathTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            string scanPath = scanPathTextBox.Text;
+            FileAttributes attribs = File.GetAttributes(scanPath);
+
+            if ((attribs & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                // Perform directory scan.
+            }
+            else
+            {
+                // Perform file scan.
+                Thread scanThread = new Thread(() =>
+                {
+                    // Signal start of scan on UI thread.
+                    this.Invoke(new Action(() =>
+                    {
+                        statusLabel.Text = "Scanning...";
+                        scanProgressBar.Value = 10;
+                    }));
+
+                    string virusName = "";
+
+                    ScanResult scanResult = _clamAV.ScanFile(scanPath, out virusName);
+
+                    // Report scan completion.
+                    this.Invoke(new Action(() =>
+                    {
+                        statusLabel.Text = "Scan complete.";
+                        scanProgressBar.Value = 100;
+
+                        logTextBox.AppendText("==========\r\n");
+                        logTextBox.AppendText("\r\n");
+                        logTextBox.AppendText("Scanning file " + scanPath + " complete.\r\n");
+                        if (scanResult == ScanResult.Clean)
+                            logTextBox.AppendText("File is clean.\r\n");
+                        else
+                            logTextBox.AppendText("File is infected with " + virusName + ".\r\n");
+                        logTextBox.AppendText("\r\n");
+                    }));
+                });
+
+                // Begin scanning.
+                scanThread.Start();
+            }
+        }
+    }
+}
