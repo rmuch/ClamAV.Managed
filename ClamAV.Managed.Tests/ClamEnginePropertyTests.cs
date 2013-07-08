780332c14a677e654acded7b7ac52bec53a9e6f5
Rupert Muchembled <rupert@rmuch.net>
2013-07-08 15:12:11 +0100

    Import unit test suite.
    * Import NUnit test suite.
    * Import NuGet infrastructure for installation of NUnit dependency,
      configured so that the executable and required packages are
      downloaded upon build.
    * Update .gitignore to ignore NuGet package path and executable.

---
+++ ClamAV.Managed.Tests/ClamEnginePropertyTests.cs
@@ -0,0 +1,197 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClamAV.Managed.Tests
+{
+    /// <summary>
+    /// Test getting and setting ClamEngine property values.
+    /// </summary>
+    [TestFixture]
+    public class ClamEnginePropertyTests
+    {
+        private ClamEngine _clamEngine;
+
+        [SetUp]
+        public void SetUpClamEngine()
+        {
+            _clamEngine = new ClamEngine();
+        }
+
+        [TearDown]
+        public void TearDownClamEngine()
+        {
+            _clamEngine.Dispose();
+            _clamEngine = null;
+        }
+
+        [Test]
+        public void VersionPropertyIsNotNullOrEmpty()
+        {
+            var version = _clamEngine.Version;
+
+            Assert.IsFalse(string.IsNullOrEmpty(version));
+        }
+
+        [Test]
+        public void DatabaseDirectoryPropertyIsNotNullOrEmpty()
+        {
+            var databaseDirectory = _clamEngine.DatabaseDirectory;
+
+            Assert.IsFalse(string.IsNullOrEmpty(databaseDirectory));
+        }
+
+        [Test]
+        public void MaxScanSizeIsReadWritable()
+        {
+            ulong value = 1024 * 123;
+
+            _clamEngine.MaxScanSize = value;
+
+            Assert.AreEqual(value, _clamEngine.MaxScanSize);
+        }
+
+        [Test]
+        public void MaxFileSizeIsReadWritable()
+        {
+            ulong value = 1024 * 124;
+
+            _clamEngine.MaxFileSize = value;
+
+            Assert.AreEqual(value, _clamEngine.MaxFileSize);
+        }
+
+        [Test]
+        public void MaxRecursionIsReadWritable()
+        {
+            uint value = 125;
+
+            _clamEngine.MaxRecursion = value;
+
+            Assert.AreEqual(value, _clamEngine.MaxRecursion);
+        }
+
+        [Test]
+        public void MaxFilesIsReadWritableIsReadWritable()
+        {
+            uint value = 126;
+
+            _clamEngine.MaxFiles = value;
+
+            Assert.AreEqual(value, _clamEngine.MaxFiles);
+        }
+
+        [Test]
+        public void MinCCCountIsReadWritableIsReadWritable()
+        {
+            uint value = 3;
+
+            _clamEngine.MinCCCount = value;
+
+            Assert.AreEqual(value, _clamEngine.MinCCCount);
+        }
+
+        [Test]
+        public void MinSSNCountIsReadWritableIsReadWritable()
+        {
+            uint value = 4;
+
+            _clamEngine.MinSSNCount = value;
+
+            Assert.AreEqual(value, _clamEngine.MinSSNCount);
+        }
+
+        [Test]
+        public void PuaCategoriesIsReadWritable()
+        {
+            string value = "xyz";
+
+            _clamEngine.PuaCategories = value;
+
+            Assert.AreEqual(value, _clamEngine.PuaCategories);
+        }
+
+        [Test]
+        public void ACOnlyIsReadWritable()
+        {
+            uint value = 1;
+
+            _clamEngine.ACOnly = value;
+
+            Assert.AreEqual(value, _clamEngine.ACOnly);
+        }
+
+        [Test]
+        public void ACMinDepthIsReadWritable()
+        {
+            uint value = 4;
+
+            _clamEngine.ACMinDepth = value;
+
+            Assert.AreEqual(value, _clamEngine.ACMinDepth);
+        }
+
+        [Test]
+        public void ACMaxDepthIsReadWritable()
+        {
+            uint value = 5;
+
+            _clamEngine.ACMaxDepth = value;
+
+            Assert.AreEqual(value, _clamEngine.ACMaxDepth);
+        }
+
+        [Test]
+        public void TempDirIsReadWritable()
+        {
+            string value = @"E:\Temp";
+
+            _clamEngine.TempDir = value;
+
+            Assert.AreEqual(value, _clamEngine.TempDir);
+        }
+
+        [Test]
+        public void KeepTempFilesIsReadWritable()
+        {
+            uint value = 0;
+
+            _clamEngine.KeepTempFiles = value;
+
+            Assert.AreEqual(value, _clamEngine.KeepTempFiles);
+        }
+
+        [Test]
+        public void BytecodeSecurityIsReadWritable()
+        {
+            var value = BytecodeSecurity.TrustAll;
+
+            _clamEngine.BytecodeSecurity = value;
+
+            Assert.AreEqual(value, _clamEngine.BytecodeSecurity);
+        }
+
+        [Test]
+        public void BytecodeTimeoutIsReadWritable()
+        {
+            uint value = 3600;
+
+            _clamEngine.BytecodeTimeout = value;
+
+            Assert.AreEqual(value, _clamEngine.BytecodeTimeout);
+        }
+
+        [Test]
+        public void BytecodeModeIsReadWritable()
+        {
+            var value = BytecodeMode.Interpreter;
+
+            _clamEngine.BytecodeMode = value;
+
+            Assert.AreEqual(value, _clamEngine.BytecodeMode);
+        }
+    }
+}
---
+++ ClamAV.Managed.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClamAV.Managed.Tests")]
+[assembly: AssemblyDescription("Managed bindings for ClamAV")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClamAV.Managed.Tests")]
+[assembly: AssemblyCopyright("Copyright © Rupert Muchembled 2011, 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("124bbc23-92e3-4fc2-884d-233d4a63cfb5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
