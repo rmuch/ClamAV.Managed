203e8bdb5b7e115e8b94cf1bb414d65f74788b2b
Rupert Muchembled <rupert@rmuch.net>
2014-03-05 14:24:23 +0000

    Update copyright header in ClamEnginePropertyTests.cs.

--- ClamAV.Managed.Tests/ClamEnginePropertyTests.cs
+++ ClamAV.Managed.Tests/ClamEnginePropertyTests.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * ClamAV.Managed.Tests - Managed bindings for ClamAV - unit test suite
- * Copyright (C) 2011, 2013 Rupert Muchembled
+ * Copyright (C) 2011, 2013-2014 Rupert Muchembled
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
