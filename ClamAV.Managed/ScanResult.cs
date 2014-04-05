c8212cfe27ebcaa6b6324dd1751ba0d57f29556e
Rupert Muchembled <rupert@rmuch.net>
2014-04-05 22:52:50 +0100

    Update year to 2014 in ClamAV.Managed copyright headers.

--- ClamAV.Managed/ScanResult.cs
+++ ClamAV.Managed/ScanResult.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * ClamAV.Managed - Managed bindings for ClamAV
- * Copyright (C) 2011, 2013 Rupert Muchembled
+ * Copyright (C) 2011, 2013-2014 Rupert Muchembled
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
